eaedc8f55f2552a68433ce99bd8305dac309e274
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-07 02:11:55 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,37 +11,13 @@ const Home: NextPage = () => {
   const address = useAddress();
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-      // Set a timeout for 2 seconds
-      const timer = setTimeout(() => {
-          setIsLoading(false);
-      }, 5000);
 
-      // Cleanup the timer when the component is unmounted
-      return () => clearTimeout(timer);
-  }, []);
-
-  if(isLoading) {
-      return (
-          <div className={styles.pageLoading}>
-              <div>
-                  <Lottie
-                      animationData={loadingLottie}
-                      loop={true}
-                  />
-              </div>
-          </div>
-      );
-  };
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.statusContainer}>
-          <UserStatus />
-        </div>
-        <h3>Status Feed:</h3>
-        <StatusEvents />
+        hello
       </div>
     </main>
   );
